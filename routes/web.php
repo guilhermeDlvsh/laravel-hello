f3e54748fcf4feaf03b8e89bbbb80349ba61a919
Guilherme Duarte <gui.duarte123@outlook.com>
2022-01-17 21:56:21 -0300

    Adicionado view para rota inicial

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HelloController::class, 'index']);
+
+
+
+// Route::get('/mazza', function () {
+//     return 'MazzaFC';
+// });
+
+?>
